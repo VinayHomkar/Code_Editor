aaa80bfa314924f8cad4fff42c67ebdc2e80030a
Vinay Homkar <homkar2015@gmail.com>
2023-12-17 17:05:12 +0530

    alignment

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -1,12 +1,19 @@
 import Editor from "./Editor";
+import { Box, styled } from "@mui/material";
+
+const Container = styled(Box)`
+  display: flex;
+  background-color: #060606;
+  height: 60vh;
+`;
 
 const Code = () => {
   return (
-    <>
-      <Editor />
-      <Editor />
-      <Editor />
-    </>
+    <Container>
+      <Editor heading="HTML" icon="<>" color="#FF3C41" />
+      <Editor heading="CSS" icon="*" color="#0EBEFF" />
+      <Editor heading="JavaSript" icon="()" color="#FCD000" />
+    </Container>
   );
 };
 
